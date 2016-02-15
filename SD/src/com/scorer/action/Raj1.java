f195538bc719cfc1e2414290e0497fe36977dc09
sudhir9031 <user.name>
2016-02-15 15:29:15 +0530

    hi

---
+++ SD/src/com/scorer/action/Raj1.java
@@ -0,0 +1,15 @@
+package com.scorer.action;
+
+public class Raj1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Raj1");
+
+	}
+
+}
---
+++ SD/src/com/scorer/action/Sudhir1.java
@@ -0,0 +1,12 @@
+
+package com.scorer.action;
+
+public class Sudhir1 {
+	
+	public static void main(String arg[])
+	{
+		System.out.println("sudhir1");
+		System.out.println("Hello");
+	}
+
+}
