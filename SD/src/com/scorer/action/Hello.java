5e5d90e6f4958f4653f8f5bf265a948f7775cda0
sudhir9031 <user.name>
2016-02-15 12:11:42 +0530

    share

---
+++ SD/src/com/scorer/action/Hello.java
@@ -0,0 +1,14 @@
+package com.scorer.action;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+	}
+
+}
